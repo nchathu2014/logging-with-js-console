1a323d72a04f2d5ef1df8458086169e4231c1789
Chathuranga <UCHATNU@PEROOT.COM>
2016-05-23 14:05:05 +0530

    initial commit

---
+++ 1-Log Levels with Symentics/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by UCHATNU on 5/23/2016.
+ */
+
+console.log("I am a log");
+console.info("I am a info message");
+console.warn("I am a warn");
+console.error("I am an error!");
+console.debug("I am a debug message");
---
+++ 2-Advanced Console Log Arguments/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by UCHATNU on 5/23/2016.
+ */
+
+console.log("I am a log message");
+
+console.log("I","am","a","log","message");
+
+console.log("My name is %s and my age is %d and I have %o ","Nuwan",100,{marks:"95%"});
+
+console.log("My name is %coutput ","color:red;font-size:20px")
